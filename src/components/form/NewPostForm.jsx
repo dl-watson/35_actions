c4c12106b02e89a77661da7ee132ecd354fc9cde
dl-watson <dlarkinwatson@gmail.com>
2021-02-17 14:38:42 -0800

    set up basic state, handleSubmit for form submission

--- src/components/form/NewPostForm.jsx
+++ src/components/form/NewPostForm.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles/NewPostForm.css";
 
 const NewPostForm = () => {
+  // refactor into a separate hook after wiring up the reducer
+  // set up useDispatch()
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { elements } = e.target;
+
+    setTitle(elements.title.value);
+    setBody(elements.body.value);
+
+    // dispatch (createPost({title, body}))
+  };
+
   return (
-    <section className={styles.NewPostForm}>
+    <section className={styles.NewPostForm} onSubmit={handleSubmit}>
       <h2>Create Post</h2>
       <form>
-        <input name="title" placeholder="Title..." />
-        <textarea name="body" placeholder="Body..." />
+        <input name="title" value={title} placeholder="Title..." />
+        <textarea name="body" value={body} placeholder="Body..." />
         <button>Submit</button>
       </form>
     </section>
