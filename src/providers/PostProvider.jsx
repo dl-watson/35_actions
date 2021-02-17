592aa79006f195cc5712a8a5791b78cfc9cab0e6
dl-watson <dlarkinwatson@gmail.com>
2021-02-17 14:29:32 -0800

    set up an empty reducer

--- src/providers/PostProvider.jsx
+++ src/providers/PostProvider.jsx
@@ -1,7 +1,10 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import { initialState, postReducer } from "../reducers/postReducer";
 
 const PostContext = createContext(null);
 
 export const PostProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(postReducer, initialState);
+
   return <PostContext.Provider>{children}</PostContext.Provider>;
 };
