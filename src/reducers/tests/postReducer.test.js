c7fbee317fde60d0f00dd3f34c8025b7cefb131e
dl-watson <dlarkinwatson@gmail.com>
2021-02-17 17:05:02 -0800

    added test for DELETE_POST reducer case

--- src/reducers/tests/postReducer.test.js
+++ src/reducers/tests/postReducer.test.js
@@ -23,4 +23,35 @@ describe("postReducer", () => {
       ],
     });
   });
+
+  it("deletes a new post with the DELETE_POST action", () => {
+    const state = {
+      posts: [
+        {
+          id: 1,
+          title: "title",
+          body: "body",
+        },
+        {
+          id: 2,
+          title: "title",
+          body: "body",
+        },
+      ],
+    };
+
+    const action = deletePost(1);
+
+    const updated = postReducer(state, action);
+
+    expect(updated).toEqual({
+      posts: [
+        {
+          id: 2,
+          title: "title",
+          body: "body",
+        },
+      ],
+    });
+  });
 });
