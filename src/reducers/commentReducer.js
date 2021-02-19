8787d3f30818af4060a8665fffd6a871ddd444ca
dl-watson <dlarkinwatson@gmail.com>
2021-02-18 16:59:46 -0800

    pass initialState into reducers

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -4,7 +4,7 @@ export const initialState = {
   comments: [],
 };
 
-export const commentReducer = (state, action) => {
+export const commentReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_COMMENT:
       return {
